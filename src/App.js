81be4c10f48e62fa6e43219e97d823c14d2bd8d2
EsotericZ <cjsand03@gmail.com>
2023-07-03 09:17:12 -0600

    user authentication

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/home/Home';
 import { Layout } from './components/Layout';
 import { Login } from './components/login/Login';
 import { Missing } from './components/missing/Missing';
+import { Unauthorized } from './components/unauthorized/Unauthorized';
 
 export const App = () => {
     return (
@@ -14,9 +15,10 @@ export const App = () => {
 
                 {/* PUBLIC ROUTES */}
                 <Route path='/login' element={<Login />} />
+                <Route path='/unauthorized' element={<Unauthorized />} />
 
                 {/* PROTECTED ROUTES */}
-                <Route element={<RequiredAuth />}>
+                <Route element={<RequiredAuth allowedRoles={[ 'admin' ]} />}>
                     <Route path='/home' element={<Home />} />
                 </Route>
 
