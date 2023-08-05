837b0bf3dd5557024f072143f7a80178ecb35612
EsotericZ <cjsand03@gmail.com>
2023-08-05 12:04:15 -0600

    new way of login and auth

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,29 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import './home.css';
+import getAllUsers from '../../services/users/getAllUsers';
 
 import useRefreshToken from '../../hooks/useRefresh';
+import { useEffect } from 'react';
 
 export const Home = () => {
     const refresh = useRefreshToken();
+    const fetchData = async () => {
+        try {
+            const getUsers = await getAllUsers()
+            .then((res) => {
+                console.log(res)
+            })
+        } catch (err) {
+            console.error(err);
+        }
+        
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+    
 
     return(
         <Box m={2}>
--- src/services/users/getAllUsers.js
+++ src/services/users/getAllUsers.js
@@ -1,12 +1,16 @@
 import api from '../api';
 // import useApiPrivate from '../../hooks/useApiPrivate';
-
+const controller = new AbortController();
 
 const getAllUsers = async () => {
-    // const apiPrivate = useApiPrivate();
-    console.log('hit')
-    // const res = await apiPrivate.get('/users/getAllUsers');
-    // return res.data
+    try {
+        const response = await api.get('/users', {
+            signal: controller.signal
+        })
+        return response.data
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 export default getAllUsers;
