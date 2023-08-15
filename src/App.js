be2fa06426b02f75b603ff3ba56d5a3b444a1907
EsotericZ <cjsand03@gmail.com>
2023-08-14 21:31:29 -0600

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Admin } from './components/admin/Admin';
+import { Blog } from './components/blog/Blog';
 import { Dashboard } from './components/dashboard/Dashboard';
 import { Director } from './components/director/Director';
+import { Events } from './components/events/Events';
 import { Home } from './components/home/Home';
 import { Layout } from './components/Layout';
 import { Login } from './components/Login';
@@ -10,6 +12,7 @@ import { Missing } from './components/missing/Missing';
 import { Navbar } from './components/navbar/Navbar';
 import { PersistLogin } from './components/PersistLogin';
 import { Profile } from './components/profile/Profile';
+import { Rankings } from './components/rankings/Rankings';
 import { Register } from './components/register/Register';
 import { RequireAuth } from './components/RequireAuth';
 import { Unauthorized } from './components/unauthorized/Unauthorized';
@@ -34,7 +37,10 @@ export const App = () => {
 
                     <Route element={<PersistLogin />}>
                         <Route path="/" element={<Home />} />
+                        <Route path="/blog" element={<Blog />} />
+                        <Route path="/events" element={<Events />} />
                         <Route path="/home" element={<Home />} />
+                        <Route path="/ranking" element={<Rankings />} />
 
                         <Route element={<RequireAuth allowedRoles={[ROLES.Player]} />}>
                             <Route path="/dashboard" element={<Dashboard />} />
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,37 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
 export const Home = () => {
     return (
-        <section>
-            <h1>Home</h1>
-            <br />
-            <h1>Welcome to NEMESIS RB</h1>
-            <br />
-        </section>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={8}>
+                    1
+                    {/* <Item>xs=6 md=8</Item> */}
+                </Grid>
+                <Grid item xs={6} md={4}>
+                    3
+                    {/* <Item>xs=6 md=4</Item> */}
+                </Grid>
+                <Grid item xs={6} md={4}>
+                    4
+                    {/* <Item>xs=6 md=4</Item> */}
+                </Grid>
+                <Grid item xs={6} md={8}>
+                    6
+                    {/* <Item>xs=6 md=8</Item> */}
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
