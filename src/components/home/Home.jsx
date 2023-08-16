d64a61a133a3ebcfb334e5d489358bd7542929f2
EsotericZ <cjsand03@gmail.com>
2023-08-15 21:35:05 -0600

    homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,37 +1,102 @@
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import './home.css';
 
 export const Home = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={6} md={8}>
-                    1
-                    {/* <Item>xs=6 md=8</Item> */}
+        <Box sx={{ flexGrow: 1 }} className='homePaper'>
+            <Grid container spacing={5}>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 140 }}
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
                 </Grid>
-                <Grid item xs={6} md={4}>
-                    3
-                    {/* <Item>xs=6 md=4</Item> */}
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    4
-                    {/* <Item>xs=6 md=4</Item> */}
-                </Grid>
-                <Grid item xs={6} md={8}>
-                    6
-                    {/* <Item>xs=6 md=8</Item> */}
-                </Grid>
+
+                <Grid item xs={12} sm={6} md={3}>
+
+                    <div class="example-2 card">
+                        <div class="wrapper">
+                            <div class="header">
+                                <div class="date">
+                                    <span class="day">12</span>
+                                    <span class="month">Aug</span>
+                                    <span class="year">2016</span>
+                                </div>
+                                <ul class="menu-content">
+                                    <li>
+                                        <a href="#" class="fa fa-bookmark-o"></a>
+                                    </li>
+                                    <li><a href="#" class="fa fa-heart-o"></a></li>
+                                    <li><a href="#" class="fa fa-comment-o"></a></li>
+                                </ul>
+                            </div>
+                            <div class="data">
+                                <div class="content">
+                                    <span class="author">Jane Doe</span>
+                                    <h1 class="title">Stranger Things: The sound of the Upside Down</h1>
+                                    <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+                                    <a href="#" class="button">Read more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                {/* <Card sx={{ maxWidth: 345 }} className='example-2'>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card> */}
             </Grid>
-        </Box>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>1</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>1</Paper>
+            </Grid>
+        </Grid>
+        </Box >
     )
 }
